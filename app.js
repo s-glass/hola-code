ad67fba864f67a08accd8a821d3d9c00e64b21a3
SARAH GLASS <SGLASS87@GMAIL.COM>
2023-01-12 12:57:31 -0800

    update JS to cat page

---
+++ app.js
@@ -0,0 +1,47 @@
+    //  <script>
+    //         alert("I hope you like cats!"); // popup box
+
+    //         let catsYN = prompt("Do you have a cat?");
+    //         if (catsYN == "yes") {
+    //         alert("Good for you!");
+    //         } else if (catsYN == "YES") {
+    //         alert("Good for you!");
+    //         } else if (catsYN == "Yes") {
+    //         alert("Good for you!");
+    //         } else {
+    //         alert("Oh, I'm sorry!");
+    //         }
+            
+    //         let catsName = prompt("What's your favorite cat's name?");
+    //         document.write("I think Pixel would like " +catsName + "!");
+    // </script>
+
+
+    alert("I hope you like cats!"); 
+
+    function catsQ1(){
+        let catsYN = prompt("Do you have a cat?");
+        console.log(catsYN, + " the user does(n't) have a cat");
+        if (catsYN == "yes") {
+        alert("Good for you!");
+        } else if (catsYN == "YES") {
+        alert("Good for you!");
+        } else if (catsYN == "Yes") {
+        alert("Good for you!");
+        } else {
+        alert("Oh, I'm sorry!");
+        }
+    }
+    function catsQ2() {
+        let catsName = prompt("What's your favorite cat's name?");
+        if (catsName){
+            console.log(catsName);
+            document.write("I think Pixel would like " +catsName + "!");
+        } else {
+            console.log ("That's unacceptable...");
+            catsName = prompt("Come on, I know you can list one cat's name.")
+            console.log(catsName);
+            document.write("I think Pixel would like " +catsName + "!");
+        }
+    console.log(catsName);
+    }
